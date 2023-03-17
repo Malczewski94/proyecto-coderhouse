7af7df732885d2ebe8a91261c5a78bd7f0a451fd
Malczewski94 <tonnymalczewski01@gmail.com>
2023-03-17 19:34:27 -0300

    tablist-test

---
+++ js/tablist.js
@@ -0,0 +1,17 @@
+"use stricy"
+
+const li        = document.querySelectorAll(".li")
+const block     = document.querySelectorAll(".block")
+
+li.forEach( ( cadaLi, i )=>{
+    li[i].addEventListener('click', ()=>{
+
+        li.forEach( ( cadaLi, i)=>{
+            li[i].classList.remove('active')
+            block[i].classList.remove('active')
+        })
+
+        li[i].classList.add('active')
+        block[i].classList.add('active')
+    })
+})
